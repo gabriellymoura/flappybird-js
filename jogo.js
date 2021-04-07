110f2190abb36ccdb4bfebe806535630351428df
Gabrielly Moura <gabriellymouta@gmail.com>
2021-04-07 11:29:28 -0300

    Adicionando colisões, efeitos e restart

--- jogo.js
+++ jogo.js
@@ -1,5 +1,11 @@
 console.log('Flappy Bird');
 
+const somHit = new Audio();
+somHit.src = './efeitos/efeitos_hit.wav';
+
+const somPula = new Audio();
+somPula.src = './efeitos/efeitos_pulo.wav';
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -63,41 +69,133 @@ const chao = {
   },
 };
 
-const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
-  gravidade: 0.25,
-  velocidade:0,
 
+const gameStart = {
+  sX: 134,
+  sY: 0,
+  w: 174,
+  h: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
   desenha() {
     contexto.drawImage(
       sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
+      gameStart.sX, gameStart.sY,
+      gameStart.w, gameStart.h,
+      gameStart.x, gameStart.y,
+      gameStart.w, gameStart.h
     );
-  },
+  }
+}
+function colisao(flappyBird, chao){
+  const flappybirdY = flappyBird.y +flappyBird.altura;
+  const chaoY = chao.y;
 
-  atualiza(){
-    flappyBird.velocidade =  flappyBird.velocidade + flappyBird.gravidade;
-    //flappyBird.x = flappyBird.x + 1;
-    flappyBird.y = flappyBird.y + flappyBird.velocidade;
+  if(flappybirdY >= chaoY){
+    return true;
+  }
+  return false;
+}
+
+function criaFlappyBird(){
+  const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+    gravidade: 0.25,
+    velocidade:0,
+    pulo:4.6,
+  
+    desenha() {
+      contexto.drawImage(
+        sprites,
+        flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+        flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+        flappyBird.x, flappyBird.y,
+        flappyBird.largura, flappyBird.altura,
+      );
+    },
+  
+    atualiza(){
+      if(colisao(flappyBird,chao)){
+        console.log('fez colisão');
+        somHit.play();
+        mudaTela(telas.INICIO);
+        return;
+      }
+  
+      flappyBird.velocidade =  flappyBird.velocidade + flappyBird.gravidade;
+      //flappyBird.x = flappyBird.x + 1;
+      flappyBird.y = flappyBird.y + flappyBird.velocidade;
+    },
+    pula(){
+      somPula.play();
+      flappyBird.velocidade = -flappyBird.pulo;
+  
+    },
+  }
+  return flappyBird;
+}
+
+const globais = {};
+let telaAtiva = {};
+function mudaTela(novatela){
+  telaAtiva= novatela;
+
+  if(telaAtiva.inicializa){
+    telaAtiva.inicializa();
+  }
+}
+
+const telas = {
+  INICIO: {
+    inicializa(){
+      globais.flappyBird = criaFlappyBird();
+    },
+    desenha(){
+      planoDeFundo.desenha();
+      chao.desenha();
+      globais.flappyBird.desenha();
+
+      gameStart.desenha();
+    },
+    click(){
+      mudaTela(telas.JOGO);
+    },
+    atualiza(){
+
+    }
   },
+  JOGO:{
+    desenha(){
+      planoDeFundo.desenha();
+      chao.desenha();
+      globais.flappyBird.desenha();
+    },
+    click(){
+      globais.flappyBird.pula();
+    },
+    atualiza(){
+      globais.flappyBird.atualiza();
+    }
+  }
 }
 
 function loop() {
-  flappyBird.atualiza();
-
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
   
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function(){
+  if(telaAtiva.click){
+    telaAtiva.click();
+  }
+})
+
+mudaTela(telas.INICIO);
 loop();
